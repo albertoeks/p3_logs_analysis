98d476b931f467ae2a051780294ef8d77fb1383e
Alberto <eks.alberto@gmail.com>
2018-05-05 20:19:53 -0300

    add third query on reports

--- reports.py
+++ reports.py
@@ -22,10 +22,35 @@ def query2():
     return results
 
 def print_result_query2():
-    print(" 2. Who are the most popular article authors of all time? ")
+    print("\n 2. Who are the most popular article authors of all time? ")
     for name, views in query2():
         print(" {} -- {} views".format(name, views))
 
+def query3():
+    db = psycopg2.connect(dbname="news")
+    c = db.cursor()
+    c.execute(
+    """
+    select cast(day as date), percentage
+    from
+        (select errors.day, round((total_errors/total_requests)*100, 2) as percentage
+            from
+                (select substring(cast(time as text),0,11) as day, cast(count(status) as decimal) as total_errors
+                    from log where status like '%404%' group by day) as errors, -- subquery total errors
+                (select substring(cast(time as text),0,11) as day, cast(count(status) as decimal) as total_requests
+                    from log group by day) as requests -- subquery total requests
+            where errors.day = requests.day) as result
+    where percentage > 1;
+    """)
+    results = c.fetchall()
+    db.close()
+    return results
+
+def print_result_query3():
+    print("\n 3. Who are the most popular article authors of all time? ")
+    for day, percentage in query3():
+        print(" {:%B %d, %Y} -- {}% errors".format(day, percentage))
 
 print_result_query1()
 print_result_query2()
+print_result_query3()
